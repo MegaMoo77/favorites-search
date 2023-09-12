a8f7d9bcf10bc35af958daf1bc9097d1e3b7fab4
UndertowTruck <123440606+UndertowTruck@users.noreply.github.com>
2023-09-12 14:05:59 -0700

    Separated custom requests functionality into custom-requests.js

---
+++ custom-requests.js
@@ -0,0 +1,138 @@
+// Handles user made page requests
+// handles custom page requests (based on failedPageRequestHandler)
+async function customPageRequestHandler(userID, pageNum) {
+	const list = document.getElementById('customRequestList')
+
+	// add button list item element to handle custom request
+	const listItem = document.createElement('li')
+	listItem.style.position = 'relative'
+
+	const button = document.createElement('button')
+	button.type = "button"
+	button.classList.add('rerequest')
+	// should be pending since request has just been made and didn't fail yet
+	button.classList.add('pending')
+	button.disabled = true
+	
+	const label = document.createElement('label')
+	label.style = "position: absolute; padding: 4px; margin-left: 10px"
+	label.textContent = `Currently rerequesting page ${pageNum} for user ${userID}`
+
+	listItem.appendChild(button)
+	listItem.appendChild(label)
+
+	list.appendChild(listItem)
+
+	// now wait for response
+	// once doc requested
+	await requestPage(userID, pageNum, maxRequestAttempts, globalAbortSignal).then(async doc => {
+		if (doc == null) {
+			// set to failure state
+			button.classList.add('failure')
+			button.classList.remove('pending')
+			label.textContent = `Failed to request page ${pageNum} for user ${userID}`
+			button.disabled = false
+		} else {
+			button.classList.add('success')
+			button.classList.remove('pending')
+			label.textContent = `Successfully requested page ${pageNum} for user ${userID}`
+			// load entire document using currently inputted commands
+			await loadContent(doc, get_commands(document.getElementById("tags").value), advancedMode.checked, globalAbortSignal)
+			
+			// fade out element code from https://stackoverflow.com/a/33424474
+			const seconds = 2;
+			listItem.style.transition = "opacity "+seconds+"s ease";
+			listItem.style.opacity = 0;
+			setTimeout(()=> {
+				list.removeChild(listItem)
+			}, seconds*1000)
+			
+			// TODO! Create X button to remove custom page request
+			// Now add element to remove page
+			// add delete list item element
+			const deleteListItem = document.createElement('li')
+			deleteListItem.style.position = 'relative'
+			const deleteButton = document.createElement('button')
+			deleteButton.type = "button"
+			deleteButton.textContent = "X"
+			deleteButton.classList.add('remove')
+
+
+			const deleteLabel = document.createElement('label')
+			deleteLabel.style = "position: absolute; padding: 4px; margin-left: 10px"
+			deleteLabel.textContent = `Page ${pageNum} for user ${userID}`
+
+			deleteListItem.appendChild(deleteButton)
+			deleteListItem.appendChild(deleteLabel)
+
+			list.appendChild(deleteListItem)
+
+			button.addEventListener('click', async evt => {
+				// TODO! Add function here that removes page from list of custom pages
+				list.removeChild(deleteListItem);
+			})
+		}
+	})
+
+	button.addEventListener('click', async evt => {
+		//disallow double clicks
+		if (!evt.detail || evt.detail == 1) {
+			button.disabled = true
+			// change button class to show pending status
+			button.classList.add('pending')
+			button.classList.remove('failure')
+			label.textContent = `Currently rerequesting page ${pageNum} for user ${userID}`
+			await requestPage(userID, pageNum, maxRequestAttempts, globalAbortSignal).then(async doc => {
+				if (doc == null) {
+					// set to failure state
+					button.classList.add('failure')
+					button.classList.remove('pending')
+					label.textContent = `Failed to request page ${pageNum} for user ${userID}`
+					button.disabled = false
+				} else {
+					// success!
+					button.classList.add('success')
+					button.classList.remove('pending')
+					label.textContent = `Successfully requested page ${pageNum} for user ${userID}`
+		
+					// load entire document using currently inputted commands
+					console.log(doc)
+					await loadContent(doc, get_commands(document.getElementById("tags").value), advancedMode.checked, globalAbortSignal)
+					
+					// fade out element code from https://stackoverflow.com/a/33424474
+					const seconds = 2;
+					listItem.style.transition = "opacity "+seconds+"s ease";
+					listItem.style.opacity = 0;
+					setTimeout(()=> {
+						list.removeChild(listItem)
+					}, seconds*1000)
+
+					// TODO! Create X button to remove custom page request
+					// Now add element to remove page
+					// add delete list item element
+					const deleteListItem = document.createElement('li')
+					deleteListItem.style.position = 'relative'
+					const deleteButton = document.createElement('button')
+					deleteButton.type = "button"
+					deleteButton.textContent = "X"
+					deleteButton.classList.add('remove')
+
+
+					const deleteLabel = document.createElement('label')
+					deleteLabel.style = "position: absolute; padding: 4px; margin-left: 10px"
+					deleteLabel.textContent = `Page ${pageNum} for user ${userID}`
+
+					deleteListItem.appendChild(deleteButton)
+					deleteListItem.appendChild(deleteLabel)
+
+					list.appendChild(deleteListItem)
+
+					button.addEventListener('click', async evt => {
+						// TODO! Add function here that removes page from list of custom pages
+						list.removeChild(deleteListItem);
+					})
+				}
+			})
+		}
+	})
+}
